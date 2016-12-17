c9016463bb1bfe276efd0479abc35a6059d50261
maxstbn <maxstbn@gmail.com>
2016-12-17 16:55:50 +0300

    admin create

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(email: "admin@inna.ru", password: "qwerty123", password_confirmation: "qwerty123")
